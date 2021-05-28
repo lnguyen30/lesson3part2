632945969eaf9b76e84b7d04a54109b21f280643
Long Nguyen <lnguyen30@uco.edu>
2021-05-27 20:50:23 -0500

    show list of purchases

--- public/viewpage/purchase_page.js
+++ public/viewpage/purchase_page.js
@@ -16,13 +16,53 @@ export function addEventListeners(){
 
 export async function purchase_page(){
 
+    if(!Auth.currentUser){
+        Element.root.innerHTML = '<h1>Protected Page</h1>'
+        return;
+    }
+
     let html = '<h1>Purchase Page</h1>';
     let carts;
+
+
     try{
         carts = await FirebaseController.getPurchaseHistory(Auth.currentUser.uid);
+        if(carts.length == 0){
+            html += '<h2>No purchase history found</h2>'
+            Element.root.innerHTML = html;
+            return;
+        }
     }catch(e){
         if(Constant.DEV) console.log(e);
         Util.info('Error in getPurchaseHistory', JSON.stringify(e));
     }
+
+    html += `
+    <table class="table table-striped">
+    <thead>
+        <tr>
+        <th scope="col">View</th>
+        <th scope="col">Items</th>
+        <th scope="col">Price</th>
+        <th scope="col">Date</th>
+        </tr>
+  </thead>
+  <tbody>
+    `;
+
+    for(let i = 0; i< carts.length; i++){
+        html +=`
+        <tr>
+            <td>
+                <button class="btn btn-outline-primary">Details</button>
+            </td>
+            <td>${carts[i].getTotalQty()}</td>
+            <td>${Util.currency(carts[i].getTotalPrice())}</td>
+            <td>${Date(carts[i].timestamp).toString()}</td>
+        </tr>
+        `;
+    }
+
+    html += '</tbody></table>'
     Element.root.innerHTML = html
 }
